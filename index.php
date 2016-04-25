c7105489dc223accef676da50e241ed4b99126e7
fanghang <fanghang@fujiacaifu.com>
2016-04-25 09:59:35 +0800

    ad

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('APPLICATION_PATH', dirname(__FILE__));
-ini_set('yaf.environ', 'develop');
-$application = new \Yaf\Application( APPLICATION_PATH . "/conf/application.ini");
-
-$application->bootstrap()->run();
-?>
