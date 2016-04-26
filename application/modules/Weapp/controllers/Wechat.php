d8315c56c9e2bb406d00bdff3727ef27d4769fed
fanghang <fanghang@fujiacaifu.com>
2016-04-26 18:07:02 +0800

    update

--- application/modules/Weapp/controllers/Wechat.php
+++ application/modules/Weapp/controllers/Wechat.php
@@ -7,7 +7,34 @@ class WechatController extends Yaf_Controller_Abstract {
         header('location: /');
     }
     
+    private function valid()
+    {
+        $echoStr = $_GET["echostr"];        //随机字符串
+        
+        if($this->checkSignature()){
+            echo $echoStr;
+            exit;
+        }
+    }
+    
+    private function checkSignature()
+    {
+        $signature = $_GET["signature"];    //微信加密签名
+        $timestamp = $_GET["timestamp"];    //时间戳
+        $nonce = $_GET["nonce"];            //随机数
+        $token = WX_TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr);      //进行字典序排序
+        //sha1加密后与签名对比
+        if( sha1(implode($tmpArr)) == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
     public function messageAction(){
+        $this->valid();exit;
         $data = file_get_contents('php://input');
         /**
          * 微信消息结构
