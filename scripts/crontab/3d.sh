83b4f57c96f79569eb5cb819d87cbb50d52ecef3
fanghang <fanghang@fujiacaifu.com>
2016-04-27 13:52:33 +0800

    update

--- scripts/crontab/3d.sh
+++ scripts/crontab/3d.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd /home/wwwroot/yaf/
-/usr/local/php7/bin/php cli.php request_uri="/jobnew/fc3d" >> ./application/logs/Job/3d.log
+/usr/local/php7/bin/php cli.php request_uri="/jobnew/fc3d" >> ./logs/Job/3d.log
--- scripts/crontab/qxc.sh
+++ scripts/crontab/qxc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd /home/wwwroot/yaf/
-/usr/local/php7/bin/php cli.php request_uri="/jobnew/qxc" >> ./application/logs/Job/qxc.log
+/usr/local/php7/bin/php cli.php request_uri="/jobnew/qxc" >> ./logs/Job/qxc.log
