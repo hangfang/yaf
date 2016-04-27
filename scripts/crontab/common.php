e9f039647eb78d79c8aff6aab2dca45853e71424
fanghang <fanghang@fujiacaifu.com>
2016-04-27 13:48:13 +0800

    update

--- scripts/crontab/common.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('APPLICATION_PATH') && exit('No direct script access allowed');
-
-date_default_timezone_set('Asia/Shanghai');
-mb_internal_encoding('UTF-8');
-$app = new Yaf_Application(APPLICATION_PATH . '/conf/application.ini');
-$app->bootstrap();
-
-$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
