26b12f02c7d82867516b9da2b395ce760b04eaa0
hangfang <fangh@me.com>
2016-04-28 23:03:11 +0800

    update

--- application/modules/Weapp/views/image/index.php
+++ application/modules/Weapp/views/image/index.php
@@ -68,7 +68,7 @@ include APPLICATION_PATH . '/application/views/common/weui/header.php';
 <script src="/static/weui/js/jweixin-1.1.0.js?v=2016-04-07"></script>
 <script>
     wx.config({
-        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: '<?php echo $data['appId'];?>', // 必填，公众号的唯一标识
         timestamp: '<?php echo $data['timestamp'];?>', // 必填，生成签名的时间戳
         nonceStr: '<?php echo $data['nonceStr'];?>', // 必填，生成签名的随机串
