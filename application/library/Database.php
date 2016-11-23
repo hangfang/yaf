b6b6b8e58869a2be520476896b693e1bea8c7823
hangfang <fangh@me.com>
2016-11-23 12:13:05 +0800

    Update Database.php
    支持读写分离

--- application/library/Database.php
+++ application/library/Database.php
@@ -6,7 +6,7 @@ class Database{
         
     }
     
-    public static function getInstance($default_group='default'){
+    public static function getInstance($default_group='db'){
         
         $db = NULL;
         
@@ -16,19 +16,20 @@ class Database{
 
         if(! $ocnfig = Yaf_Registry::get('db_config')){
             $config = new Yaf_Config_Ini(APPLICATION_PATH . '/conf/database.ini', ini_get('yaf.environ'));
+            $config = $config->toArray();
+            $config = $config['database'][$default_group][rand(0,count($config)-1)];
             Yaf_Registry::set('db_config', $config);
         }
         
-        $config = $config->toArray();
-        $driverName = ucfirst($config['database'][$default_group]['dbdriver']);
+        $driverName = ucfirst($config['dbdriver']);
         $driver = 'Database_Drivers_'.$driverName.'_'.$driverName.'Driver';
-        $db = new $driver($config['database'][$default_group]);
+        $db = new $driver($config);
 
         // Check for a subdriver
         if ( ! empty($db->subdriver))
         {
             $driver = 'Database_Drivers_Pdo_Subdrivers_'.ucfirst($db->dbdriver).''.ucfirst($db->subdriver).'Driver';
-            $db = new $driver($config['database'][$default_group]);
+            $db = new $driver($config);
         }
 
         $db->initialize();
