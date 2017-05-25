15c4762addcfa1c7bc3fcd78302a001c4a7b96b2
fanghang <fanghang@fujiacaifu.com>
2017-05-25 16:29:01 +0800

    添加脚本提示

---
+++ application/controllers/Base.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASE_PATH') OR exit('No direct script access allowed');
+/**
+ * @name BaseController
+ * @author fanghang@me.com
+ * @desc 默认控制器
+ * @see http://www.php.net/manual/en/class.yaf-controller-abstract.php
+ */
+class BaseController extends Yaf_Controller_Abstract {
+    public function init(){
+        if(!is_cli()){
+            
+        }else{
+            $tmp = get_class_methods($this->_request->getControllerName().'Controller');
+            $methods = array();
+            foreach($tmp as $v){
+                if(strtolower(substr($v, -6))==='action'){
+                    $methods[] = strtolower(preg_replace('/(.*)action$/i', '\1', $v));
+                }
+            }
+
+            $action = strtolower($this->_request->getActionName());
+            if(!in_array($action, $methods)){
+                exit("php: '". $action ."' is not a correct command.\n\nDid you mean one of these?\n\t".implode("\n\t", $methods)."\n");
+            }
+        }
+    }
+}
